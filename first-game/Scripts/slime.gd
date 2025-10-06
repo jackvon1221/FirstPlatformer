7769a92915fe705b355599dbb4c7279851fac4e1
Jack VonAchen <jackvonachen@gmail.com>
2025-10-06 17:14:32 -0500

    added background music and coin SFX

--- first-game/Scripts/slime.gd
+++ first-game/Scripts/slime.gd
@@ -6,11 +6,14 @@ var direction = 1
 
 @onready var raycastright: RayCast2D = $RAYCASTRIGHT
 @onready var raycastleft: RayCast2D = $RAYCASTLEFT
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if raycastright.is_colliding():
 		direction = -1
+		animated_sprite.flip_h = true
 	if raycastleft.is_colliding():
 		direction = 1
+		animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
